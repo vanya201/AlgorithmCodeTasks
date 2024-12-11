d833613093358f290844a54b459273b21c750d9f
vanya201 <lenapetrova1905@gmail.com>
2024-12-11 13:23:34 +0200

    README.md commit

---
+++ ASearchAlgorithm/ASearchalgorithm/src/test/java/Task.java
@@ -0,0 +1,47 @@
+import entitys.ComplexEdge;
+import entitys.Vertex;
+import org.jgrapht.Graph;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import task.ASearchSolution;
+import task.DijkstraSearchSolution;
+import task.Solution;
+
+
+public class Task {
+    private Solution aSearchSolution;
+    private Solution dijkstraSearchSolution;
+    private Graph<Vertex, ComplexEdge> graph;
+
+
+    @BeforeEach
+    public void setUp() {
+        this.aSearchSolution = new ASearchSolution();
+        this.dijkstraSearchSolution = new DijkstraSearchSolution();
+        initGraph();
+
+    }
+
+    private void initGraph() {
+
+    }
+
+    @Test
+    public void testASearchSolution() {
+
+    }
+
+    @Test
+    public void testDijkstraSolution() {
+
+    }
+
+
+    @AfterEach
+    public void tearDown() {
+        this.mySolution = null;
+        this.bestSolution = null;
+        graph = null;
+    }
+}
